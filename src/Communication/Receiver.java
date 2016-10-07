1b381a087aef01ebb89a212141f5b0ec19da2e64
Fadel <Fadel@Nour-PC>
2016-10-07 23:18:11 +0300

    Stop task command added, Thread added to control gantt charts in runtime

--- src/Communication/Receiver.java
+++ src/Communication/Receiver.java
@@ -20,18 +20,20 @@ public class Receiver extends Thread {
     PS_Gantt_Chart_Tasks psgct;
     PS_Gantt_Chart_VM psgcvm;
     PL_Frame plframe;
-    int counter=0;
-   
+    int counter = 0;
+
     SerialCommunicator sc;
     final String VM_GPOS = "go";
     final String VM_RTOS = "ro";
     final String VM_Service = "sv";
+    private LinkedList<String> VM_TYPE_LL;
+    private LinkedList<String> VM_PRIO_LL;
 
-    public Receiver(SerialCommunicator sc,PS_Gantt_Chart_Tasks psgct,  PS_Gantt_Chart_VM psgcvm,PL_Frame plframe) {
+    public Receiver(SerialCommunicator sc, PS_Gantt_Chart_Tasks psgct, PS_Gantt_Chart_VM psgcvm, PL_Frame plframe) {
         this.sc = sc;
         this.psgct = psgct;
-        this.psgcvm= psgcvm;
-        this.plframe= plframe;
+        this.psgcvm = psgcvm;
+        this.plframe = plframe;
     }
 
     @Override
@@ -42,25 +44,25 @@ public class Receiver extends Thread {
         //      sc.connect();
         String command;
         while (true) {
-            if (Main_Frame.connect_status==0)
+            if (Main_Frame.connect_status == 0) {
                 break;
+            }
             command = sc.getReply();
-           // System.err.println(command);
+            // System.err.println(command);
             if (command != null) {
                 counter++;
-                if (counter==500)
-                {
-                    counter=0;
+                if (counter == 500) {
+                    counter = 0;
                     resetdata();
                 }
-             //   System.err.println(command);
+                //   System.err.println(command);
                 String[] s = command.split(" ");
                 if (s.length > 0) {
                     if (s[0].equals("INIT")) {
                         int VM_count = Integer.parseInt(s[1]);
                         LinkedList<String> VM_TYPE_LL = new LinkedList<String>();
                         LinkedList<String> VM_PRIO_LL = new LinkedList<String>();
-                        for (int i = 2; i < (2+(VM_count * 2)); i++) {
+                        for (int i = 2; i < (2 + (VM_count * 2)); i++) {
                             if ((i % 2) == 0) {
                                 String type = s[i];
                                 String vm = "";
@@ -82,49 +84,45 @@ public class Receiver extends Thread {
                             }
                         }
 
-                        intialize_VM( VM_TYPE_LL, VM_PRIO_LL);
+                        intialize_VM(VM_TYPE_LL, VM_PRIO_LL);
                     } else if (s[0].equals("INFO")) {
                         switch (s[1]) {
-                            case "vm":
-                                {
-                                    String vm_id = s[2];
-                                    String sch_time = s[3];
-                                    Schedule_VM(vm_id, sch_time);
-                                    break;
-                                }
-                            case "tk":
-                                {
-                                    String vm_id = s[2];
-                                    String task_id = s[3];
-                                    String sch_time = s[4];
-                                    Schedule_Task(vm_id,task_id, sch_time);
-                                    break;
-                                }
-                                 case "ts":
-                                {
-                                    String vm_id = s[2];
-                                    String sch_time = s[3];
-                                    Stop_Task(vm_id, sch_time);
-                                    break;
-                                }
-                            case "pl":
-                                {
-                                    String pr_id = s[2];
-                                    String acc = s[3];
-                                    String state = s[4];
-                                    String vm_id = s[5];
-                                    Allocate_PL(pr_id,acc, state,vm_id);
-                                    break;
-                                }
+                            case "vm": {
+                                String vm_id = s[2];
+                                String sch_time = s[3];
+                                Schedule_VM(vm_id, sch_time);
+                                break;
+                            }
+                            case "tk": {
+                                String vm_id = s[2];
+                                String task_id = s[3];
+                                String sch_time = s[4];
+                                Schedule_Task(vm_id, task_id, sch_time);
+                                break;
+                            }
+                            case "ts": {
+                                String vm_id = s[2];
+                                String sch_time = s[3];
+                                Stop_Task(vm_id, sch_time);
+                                break;
+                            }
+                            case "pl": {
+                                String pr_id = s[2];
+                                String acc = s[3];
+                                String state = s[4];
+                                String vm_id = s[5];
+                                Allocate_PL(pr_id, acc, state, vm_id);
+                                break;
+                            }
                             case "ms":
-                                String [] splited = command.split("<");
+                                String[] splited = command.split("<");
                                 String message = splited[1];
                                 PL_message(message);
                                 break;
                             default:
                                 break;
                         }
-                        
+
                     }
                 }
             }
@@ -132,62 +130,66 @@ public class Receiver extends Thread {
         }
     }
 
-    private void intialize_VM( LinkedList<String> VM_TYPE_LL, LinkedList<String> VM_PRIO_LL) {
-      //  Main_Frame.jTextArea1.append("intialize VM command Recieved \n");
+    private void intialize_VM(LinkedList<String> VM_TYPE_LL, LinkedList<String> VM_PRIO_LL) {
+        //  Main_Frame.jTextArea1.append("intialize VM command Recieved \n");
+        this.VM_TYPE_LL = VM_TYPE_LL;
+        this.VM_PRIO_LL = VM_PRIO_LL;
+
         psgcvm.intialize(VM_TYPE_LL);
-        psgct.intialize (VM_TYPE_LL,VM_PRIO_LL);
+        psgct.intialize(VM_TYPE_LL, VM_PRIO_LL);
     }
 
     private void Schedule_VM(String vm_id, String sch_time) {
         //Main_Frame.jTextArea1.append("Schedule VM command Recieved \n");
-        psgcvm.schedule_VM(vm_id,sch_time);
+        psgcvm.schedule_VM(vm_id, sch_time);
     }
 
     private void Schedule_Task(String vm_id, String task_id, String sch_time) {
-         //Main_Frame.jTextArea1.append("Schedule Task command Recieved \n");
-         psgct.schedule_tasks(vm_id,task_id,sch_time);
+        //Main_Frame.jTextArea1.append("Schedule Task command Recieved \n");
+        psgct.schedule_tasks(vm_id, task_id, sch_time);
     }
 
     private void Allocate_PL(String pr_id, String acc, String state, String vm_id) {
-    //    Main_Frame.jTextArea1.append("Allocate PL command Received \n");
-        plframe.allocate(pr_id,acc,state,vm_id);
+        //    Main_Frame.jTextArea1.append("Allocate PL command Received \n");
+        plframe.allocate(pr_id, acc, state, vm_id);
     }
 
     private void PL_message(String message) {
-   //   Main_Frame.jTextArea1.append("PL INfO Message Received \n");
-      plframe.add_message(message);
+        //   Main_Frame.jTextArea1.append("PL INfO Message Received \n");
+        plframe.add_message(message);
     }
 
     private void Stop_Task(String vm_id, String sch_time) {
-     //  Main_Frame.jTextArea1.append("Stop Task command Recieved \n");
-         psgct.stop_tasks(vm_id, sch_time);
+        //  Main_Frame.jTextArea1.append("Stop Task command Recieved \n");
+        psgct.stop_tasks(vm_id, sch_time);
     }
 
     public void rcsettasks(PS_Gantt_Chart_Tasks ps_gantt_chart_tasks) {
-      psgct=ps_gantt_chart_tasks;
+        psgct = ps_gantt_chart_tasks;
     }
 
     public void rcsetvms(PS_Gantt_Chart_VM ps_gantt_chart_VM) {
-        psgcvm=ps_gantt_chart_VM;
+        psgcvm = ps_gantt_chart_VM;
     }
 
-    public void resetdata()
-    {
-         Point location = Main_Frame.frame.ps_gantt_chart_tasks.getLocation();
-          Point locationvm = Main_Frame.frame.ps_gantt_chart_VM.getLocation();
+    public void resetdata() {
+        Point location = Main_Frame.frame.ps_gantt_chart_tasks.getLocation();
+        Point locationvm = Main_Frame.frame.ps_gantt_chart_VM.getLocation();
         Main_Frame.frame.ps_gantt_chart_VM.dispose();
         Main_Frame.frame.ps_gantt_chart_tasks.dispose();
         Main_Frame.frame.ps_gantt_chart_VM = new PS_Gantt_Chart_VM();
-         Main_Frame.frame.ps_gantt_chart_VM.setLocation(locationvm);
-         
+        Main_Frame.frame.ps_gantt_chart_VM.setLocation(locationvm);
+
         Main_Frame.frame.ps_gantt_chart_VM.setVisible(true);
-       
+
         Main_Frame.frame.ps_gantt_chart_tasks = new PS_Gantt_Chart_Tasks();
-           Main_Frame.frame.ps_gantt_chart_tasks.setLocation(location);
+        Main_Frame.frame.ps_gantt_chart_tasks.setLocation(location);
         Main_Frame.frame.ps_gantt_chart_tasks.setVisible(true);
 
         Main_Frame.frame.ps_gantt_chart_VM.PS_Gantt_Chart_VM_Set_psgct(Main_Frame.frame.ps_gantt_chart_tasks);
-      rcsettasks( Main_Frame.frame.ps_gantt_chart_tasks);
-      rcsetvms( Main_Frame.frame.ps_gantt_chart_VM);
+        rcsettasks(Main_Frame.frame.ps_gantt_chart_tasks);
+        rcsetvms(Main_Frame.frame.ps_gantt_chart_VM);
+        psgcvm.intialize(VM_TYPE_LL);
+        psgct.intialize(VM_TYPE_LL, VM_PRIO_LL);
     }
 }
